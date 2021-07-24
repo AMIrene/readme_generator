54d280416cf9a94049775e28517fdc04eb4af9bb
AMIrene <f.adammanik@gmail.com>
2021-07-25 09:20:34 +1000

    inquirer prompts added up to installations

--- develop/index.js
+++ develop/index.js
@@ -1,6 +1,49 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'Project Title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'Project Description',
+      message: 'Provide a description of your project',
+    },
+    {
+      type: 'input',
+      name: 'Usage',
+      message: 'Provide instructions for installation',
+    },
+    {
+      type: 'input',
+      name: 'food',
+      message: 'What is your favorite food?',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username',
+    },
+    {
+      type: 'input',
+      name: 'linkedin',
+      message: 'Enter your LinkedIn URL.',
+    },
+  ])
+  .then((answers) => {
+    const htmlPageContent = generateHTML(answers);
+
+    fs.writeFile('index.html', htmlPageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created index.html!')
+    );
+  });
+
 const questions = [];
 
 // TODO: Create a function to write README file
