329eae82214b1da77520eb3377e61304c9f0e165
AMIrene <f.adammanik@gmail.com>
2021-07-25 10:01:48 +1000

    added code to generate markdown & init function added

--- develop/index.js
+++ develop/index.js
@@ -1,56 +1,70 @@
-// TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
+//function to initialise application
+function init() {
+    // TODO: Include packages needed for this application
+    const inquirer = require('inquirer');
+    const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'Project Title',
-      message: 'What is the title of your project?',
-    },
-    {
-      type: 'input',
-      name: 'Project Description',
-      message: 'Provide a description of your project',
-    },
-    {
-      type: 'input',
-      name: 'Usage',
-      message: 'Provide instructions for installation',
-    },
-    {
-      type: 'input',
-      name: 'food',
-      message: 'What is your favorite food?',
-    },
-    {
-      type: 'input',
-      name: 'github',
-      message: 'Enter your GitHub Username',
-    },
-    {
-      type: 'input',
-      name: 'linkedin',
-      message: 'Enter your LinkedIn URL.',
-    },
-  ])
-  .then((answers) => {
-    const htmlPageContent = generateHTML(answers);
+    const generateReadMe = (answers) =>
+        // TODO: Create an array of questions for user input
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'title',
+                    message: 'What is the title of your project?',
+                },
+                {
+                    type: 'input',
+                    name: 'description',
+                    message: 'Provide a description of your project',
+                },
+                {
+                    type: 'input',
+                    name: 'usage',
+                    message: 'Provide instructions on using the application',
+                },
+                {
+                    type: 'checkbox',
+                    name: 'licenses',
+                    message: 'What licenses did you use for the application?',
+                    choices: ["MIT", "GNU General Public License 2.0", "Apache License 2.0", "GNU General Public License 3.0"]
+                },
+                {
+                    type: 'input',
+                    name: 'contributions',
+                    message: 'How can other users contribute to this project?',
+                },
+                {
+                    type: 'input',
+                    name: 'tests',
+                    message: 'How can users do testing on this project?',
+                },
 
-    fs.writeFile('index.html', htmlPageContent, (err) =>
-      err ? console.log(err) : console.log('Successfully created index.html!')
-    );
-  });
+                {
+                    type: 'input',
+                    name: 'github',
+                    message: 'What is your GitHub username?',
+                },
 
-const questions = [];
+                {
+                    type: 'input',
+                    name: 'contact',
+                    message: 'What is your email address for other users to contact you?',
+                },
+            ])
+            .then((answers) => {
+                const readMeContent = generateReadMe(answers);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+                // TODO: Create a function to write README file
+                fs.writeFile('Readme.md', readMeContent, (err) =>
+                    err ? console.log(err) : console.log('Successfully created Readme.md content!')
+                );
+            });
 
-// TODO: Create a function to initialize app
-function init() {}
+}
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
 
 // Function call to initialize app
 init();
